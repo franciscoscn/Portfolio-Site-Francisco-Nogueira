41ed023371f7220ecc4113502877ec3e4133942a
franciscoscn <97798650+franciscoscn@users.noreply.github.com>
2022-02-05 10:51:24 +0100

    task 2.3 - jquery hamb. menu
    - added a hamburger menu to the mobile version of the website on all pages.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-console.log("Hello World! This is my first JavaScript script.");
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
